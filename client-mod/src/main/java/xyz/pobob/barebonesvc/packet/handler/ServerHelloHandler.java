bad212a26c7fa63e2eea00766c758462d0feb86f
Bone <bonemail100@gmail.com>
2026-07-08 19:17:11 +0800

    Resets client state after failed join request

--- client-mod/src/main/java/xyz/pobob/barebonesvc/packet/handler/ServerHelloHandler.java
+++ client-mod/src/main/java/xyz/pobob/barebonesvc/packet/handler/ServerHelloHandler.java
@@ -64,6 +64,7 @@ public class ServerHelloHandler implements ServerPacketHandler {
 
                         if (text != null) {
                             BareBonesVCClient.sendMessageSafe(text, true);
+                            BareBonesVCClient.INSTANCE.onDisconnect();
                             return;
                         }
 
